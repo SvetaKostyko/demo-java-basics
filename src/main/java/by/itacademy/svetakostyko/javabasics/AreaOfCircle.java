53479fc2d6f71dbfd8e5eec9b79e61ad029dbbe6
Sveta <skostyko@gmail.com>
2023-03-01 00:01:19 +0100

    refactor class AreaOfCircle

--- src/main/java/by/itacademy/svetakostyko/javabasics/AreaOfCircle.java
+++ src/main/java/by/itacademy/svetakostyko/javabasics/AreaOfCircle.java
@@ -1,4 +1,17 @@
 package by.itacademy.svetakostyko.javabasics;
 
+import java.util.Scanner;
+
 public class AreaOfCircle {
+    public double countArea(double r) {
+        double s = r * r * Math.PI;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        AreaOfCircle area = new AreaOfCircle();
+        Scanner scanner = new Scanner(System.in);
+        double radius = scanner.nextDouble();
+        System.out.println(area.countArea(radius));
+    }
 }
