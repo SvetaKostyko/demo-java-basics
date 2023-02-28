3f2aaf588c41e617ff1ebf39f40ceb64dae660ee
Sveta <skostyko@gmail.com>
2023-02-28 23:32:28 +0100

    refactor class IntegerRandomNumber

--- src/main/java/by/itacademy/svetakostyko/javabasics/IntegerRandomNumber.java
+++ src/main/java/by/itacademy/svetakostyko/javabasics/IntegerRandomNumber.java
@@ -1,4 +1,13 @@
 package by.itacademy.svetakostyko.javabasics;
-public class IntegerRandomNumber {
 
+public class IntegerRandomNumber {
+    public int setRandomNumber() {
+        int randomNumber = (int) (Math.random() * 100);
+        return randomNumber;
+    }
+
+    public static void main(String[] args) {
+        IntegerRandomNumber number = new IntegerRandomNumber();
+        System.out.println(number.setRandomNumber());
+    }
 }
