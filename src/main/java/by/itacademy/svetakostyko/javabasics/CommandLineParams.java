4057bd1caa0d925c98e15099e8a039f3b078ab7b
Sveta <skostyko@gmail.com>
2023-03-01 09:02:05 +0100

    refactor class CommandLineParams

--- src/main/java/by/itacademy/svetakostyko/javabasics/CommandLineParams.java
+++ src/main/java/by/itacademy/svetakostyko/javabasics/CommandLineParams.java
@@ -2,7 +2,7 @@ package by.itacademy.svetakostyko.javabasics;
 
 public class CommandLineParams {
     public static void main(String[] args) {
-        for(int i = 0; i < args.length; i++) {
+        for(int i = args.length - 1; i >= 0; i--) {
             System.out.println("Argument " + i + " = " + args[i]);
         }
     }
