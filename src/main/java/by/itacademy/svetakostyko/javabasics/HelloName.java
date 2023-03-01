3931155e0e67a34f9f55561dfa6f8405c2047171
Sveta <skostyko@gmail.com>
2023-03-01 09:05:31 +0100

    refactor class HelloName

--- src/main/java/by/itacademy/svetakostyko/javabasics/HelloName.java
+++ src/main/java/by/itacademy/svetakostyko/javabasics/HelloName.java
@@ -1,4 +1,11 @@
 package by.itacademy.svetakostyko.javabasics;
 
+import java.util.Scanner;
+
 public class HelloName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
 }
