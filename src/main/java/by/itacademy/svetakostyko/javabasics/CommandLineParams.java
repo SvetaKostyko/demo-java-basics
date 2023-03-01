301988776817abe001b9ede00bcdd87d846f5848
Sveta <skostyko@gmail.com>
2023-03-01 08:59:11 +0100

    add class CommandLineParams

---
+++ src/main/java/by/itacademy/svetakostyko/javabasics/CommandLineParams.java
@@ -0,0 +1,9 @@
+package by.itacademy.svetakostyko.javabasics;
+
+public class CommandLineParams {
+    public static void main(String[] args) {
+        for(int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + " = " + args[i]);
+        }
+    }
+}
